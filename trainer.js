b43ea9d74477cb2a748078df93a5fc7e8d36eb50
Frodos <40771447+FrodoTrash@users.noreply.github.com>
2023-03-14 21:51:05 +0100

    Add files via upload

--- trainer.js
+++ trainer.js
@@ -5,21 +5,23 @@ cheatsEnable = true
 function createTrainer(){
   document.write( '<div id=\"trainer\" class=\"trainer\">\n' );
   document.write( '  <div class=\"trainer-content\">\n' );
-  document.write( '    <h4>FrodoTrash Auto-Trainer</h4>\n' );
+  document.write( '    <h4>Big thanks to FrodoTrash!!! FrodoTrash&Smetana Mr.Mine Mod</h4>\n' );
   document.write( '    <span class=\"close\">X</span>\n' );
 
 
-  document.write( '    <button id="tAutoOpen" value="off" onClick="autoOpenToggle()" >auto open chests OFF</button>\n' ); //<br> breake line
-  document.write( '    <button id="tAutoSell" onClick="autoSellToggle()" >auto sell OFF</button>\n' );
-  document.write( '    <button id="tAutoMineral" onClick="autoMineralToggle()" >auto mine minerals OFF</button>\n' );
-  document.write( '    <button id="tAutoBattle" onClick="autoBattleToggle()" >auto battle OFF</button>\n' );
-  document.write( '    <button id="tAutoCompress" onClick="autoCompressToggle()" >auto compress OFF</button><br><br>\n' );
+  document.write( '    <button id="tAutoOpen" value="off" onClick="autoOpenToggle()" >open chests OFF</button>\n' ); //<br> breake line
+  document.write( '    <button id="tAutoSell" onClick="autoSellToggle()" >sell minerals OFF</button>\n' );
+  document.write( '    <button id="tAutoMineral" onClick="autoMineralToggle()" >mine minerals OFF</button>\n' );
+  document.write( '    <button id="tAutoBattle" onClick="autoBattleToggle()" >battle OFF</button>\n' );
+  document.write( '    <button id="tAutoCompress" onClick="autoCompressToggle()" >compress OFF</button><br><br>\n' );
 
-  document.write( '    <button id="tAutoCraft" onClick="autoCraftToggle()" >auto craft red OFF</button>\n' );
-  document.write( '    <button id="tAutoCraft2" onClick="autoCraftToggle2()" >auto craft blue OFF</button>\n' );
-  document.write( '    <button id="tAutoCraft3" onClick="autoCraftToggle3()" >auto craft green OFF</button>\n' );
-  document.write( '    <button id="tAutoCraft4" onClick="autoCraftToggle4()" >auto craft purple OFF</button>\n' );
-  document.write( '    <button id="tAutoCraft5" onClick="autoCraftToggle5()" >auto craft yellow OFF</button>\n' );
+  document.write( '    <button id="tAutoCraft" onClick="autoCraftToggle()" >craft red OFF</button>\n' );
+  document.write( '    <button id="tAutoCraft2" onClick="autoCraftToggle2()" >craft blue OFF</button>\n' );
+  document.write( '    <button id="tAutoCraft3" onClick="autoCraftToggle3()" >craft green OFF</button>\n' );
+  document.write( '    <button id="tAutoCraft4" onClick="autoCraftToggle4()" >craft purple OFF</button>\n' );
+  document.write( '    <button id="tAutoCraft5" onClick="autoCraftToggle5()" >craft yellow OFF</button><br><br>\n' );
+
+  document.write( '    <button id="tAutoApply" onClick="autoApplyToggle()" >superminers collect OFF</button> <span>For Goldalf, Thoth the Wise(rewards) and Diana(buff)</span> ' );
 
   document.write( '  </div>\n' );
   document.write( '</div>' );
@@ -85,122 +87,134 @@ function trainerClose(TrainerClose){
 }
 function autoSellToggle(){
   button = document.getElementById('tAutoSell')
-  if(button.innerText == "auto sell OFF"){
+  if(button.innerText == "sell minerals OFF"){
     isAutoSelling = true
     windowState[1]=1
-    button.innerText = "auto sell ON"
+    button.innerText = "sell minerals ON"
   }
   else{
     isAutoSelling = false
     windowState[1]=0
-    button.innerText = "auto sell OFF"
+    button.innerText = "sell minerals OFF"
   }
 }
 function autoOpenToggle(){
   button = document.getElementById('tAutoOpen')
-  if(button.innerText == "auto open chests OFF"){
+  if(button.innerText == "open chests OFF"){
     isAutoOpening = true
-    button.innerText = "auto open chests ON"
+    button.innerText = "open chests ON"
   }
   else{
     isAutoOpening = false
-    button.innerText = "auto open chests OFF"
+    button.innerText = "open chests OFF"
   }
 }
 
 function autoMineralToggle(){
   button = document.getElementById('tAutoMineral')
-  if(button.innerText == "auto mine minerals OFF"){
+  if(button.innerText == "mine minerals OFF"){
     isAutoMineral = true
-    button.innerText = "auto mine minerals ON"
+    button.innerText = "mine minerals ON"
   }
   else{
     isAutoMineral = false
-    button.innerText = "auto mine minerals OFF"
+    button.innerText = "mine minerals OFF"
   }
 }
 
 function autoBattleToggle(){
   button = document.getElementById('tAutoBattle')
-  if(button.innerText == "auto battle OFF"){
+  if(button.innerText == "battle OFF"){
     isAutoBattling = true
-    button.innerText = "auto battle ON"
+    button.innerText = "battle ON"
   }
   else{
     isAutoBattling = false
-    button.innerText = "auto battle OFF"
+    button.innerText = "battle OFF"
   }
 }
 
 function autoCompressToggle(){
   button = document.getElementById('tAutoCompress')
-  if(button.innerText == "auto compress OFF"){
+  if(button.innerText == "compress OFF"){
     isAutoCompressing = true
-    button.innerText = "auto compress ON"
+    button.innerText = "compress ON"
   }
   else{
     isAutoCompressing = false
-    button.innerText = "auto compress OFF"
+    button.innerText = "compress OFF"
   }
 }
 
 function autoCraftToggle(){
   button = document.getElementById('tAutoCraft')
-  if(button.innerText == "auto craft red OFF"){
+  if(button.innerText == "craft red OFF"){
     isAutoCrafting = true
-    button.innerText = "auto craft red ON"
+    button.innerText = "craft red ON"
   }
   else{
     isAutoCrafting = false
-    button.innerText = "auto craft red OFF"
+    button.innerText = "craft red OFF"
   }
 }
 
 function autoCraftToggle2(){
   button = document.getElementById('tAutoCraft2')
-  if(button.innerText == "auto craft blue OFF"){
+  if(button.innerText == "craft blue OFF"){
     isAutoCrafting2 = true
-    button.innerText = "auto craft blue ON"
+    button.innerText = "craft blue ON"
   }
   else{
     isAutoCrafting2 = false
-    button.innerText = "auto craft blue OFF"
+    button.innerText = "craft blue OFF"
   }
 }
 
 function autoCraftToggle3(){
   button = document.getElementById('tAutoCraft3')
-  if(button.innerText == "auto craft green OFF"){
+  if(button.innerText == "craft green OFF"){
     isAutoCrafting3 = true
-    button.innerText = "auto craft green ON"
+    button.innerText = "craft green ON"
   }
   else{
     isAutoCrafting3 = false
-    button.innerText = "auto craft green OFF"
+    button.innerText = "craft green OFF"
   }
 }
 
 function autoCraftToggle4(){
   button = document.getElementById('tAutoCraft4')
-  if(button.innerText == "auto craft purple OFF"){
+  if(button.innerText == "craft purple OFF"){
     isAutoCrafting4 = true
-    button.innerText = "auto craft purple ON"
+    button.innerText = "craft purple ON"
   }
   else{
     isAutoCrafting4 = false
-    button.innerText = "auto craft purple OFF"
+    button.innerText = "craft purple OFF"
   }
 }
 
 function autoCraftToggle5(){
   button = document.getElementById('tAutoCraft5')
-  if(button.innerText == "auto craft yellow OFF"){
+  if(button.innerText == "craft yellow OFF"){
     isAutoCrafting5 = true
-    button.innerText = "auto craft yellow ON"
+    button.innerText = "craft yellow ON"
   }
   else{
     isAutoCrafting5 = false
-    button.innerText = "auto craft yellow OFF"
+    button.innerText = "craft yellow OFF"
+  }
+}
+
+function autoApplyToggle(){
+  button = document.getElementById('tAutoApply')
+  if(button.innerText == "superminers collect OFF"){
+    isAutoApplying = true
+    button.innerText = "superminers collect ON"
+  }
+  else{
+    isAutoApplying = false
+    button.innerText = "superminers collect OFF"
   }
 }
 
@@ -215,6 +229,7 @@ isAutoCrafting2 = false
 isAutoCrafting3 = false
 isAutoCrafting4 = false
 isAutoCrafting5 = false
+isAutoApplying = false
 
 if(cheatsEnable){
   createTrainer()
@@ -232,6 +247,7 @@ if(cheatsEnable){
   isAutoCraft3 = document.getElementById('tAutoCraft3')
   isAutoCraft4 = document.getElementById('tAutoCraft4')
   isAutoCraft5 = document.getElementById('tAutoCraft5')
+  isAutoApply = document.getElementById('tAutoApply')
   execute()
 }
  
@@ -253,7 +269,7 @@ async function execute(){
       windowState[1] = 0
     } 
 
-    // auto mineral clicker
+    // mineral clicker
     if (isAutoMineral){
       //worldClickables array mineralov
       for(var i in worldClickables){
@@ -262,7 +278,7 @@ async function execute(){
         }
       }
     }
-    // auto chest opener
+    // chest opener
     if(isAutoOpening){
       while (chestService.chests != 0){
         chestService.chests.forEach(chest => {
@@ -270,7 +286,7 @@ async function execute(){
         })
       }
     }
-    // auto battleing
+    // battles
     if(isAutoBattling){
       if(battleWaiting != 0){
         panToViewDepth(battleWaiting[1])
@@ -314,6 +330,12 @@ async function execute(){
         GemForger.addGemToQueue(YELLOW_FORGED_GEM_INDEX)
       }
     }
-  }  
+    // super miners goodies
+    if(isAutoApplying){
+      for(superMiner of superMinerManager.currentSuperMiners){
+        superMiner.onButtonPress()
+      }
+    }  
+  }
 }
 execute()
